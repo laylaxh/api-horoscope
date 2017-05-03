87cb166cb633df9267db42ddc774672d962a4b4e
Layla Habahbeh <laylahabahbeh@lc-laylahabahbeh.local>
2017-05-03 11:41:46 -0700

    Complete API demo from Rails Conf workshop

--- horoscope.rb
+++ horoscope.rb
@@ -5,6 +5,19 @@ require 'oauth'
 require 'sinatra'
 
 get '/' do
-  @prediction = ['🍰', '💔', '💪', '🏆', '🐷', '🍕', '🤖'].sample
+  @prediction = HTTParty.get('https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=5&rnnamespace=0&format=json')
+  @prediction = @prediction["query"]["random"].first["title"]
+
   erb :index
+end
+
+post '/tweet' do
+  consumer = OAuth::Consumer.new(
+    ENV['API_KEY'], ENV['API_SECRET'],
+    { site: 'https://api.twitter.com', scheme: 'header' }
+  )
+  token_hash = { oauth_token: ENV['ACCESS_TOKEN'], oauth_token_secret: ENV['ACCESS_TOKEN_SECRET'] }
+  access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
+  access_token.request(:post, 'https://api.twitter.com/1.1/statuses/update.json', status: params[:status])
+  redirect to('/')
 end
